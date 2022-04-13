2cc8373b1837dca29d9d5a01a7c176f934dadfd0
JoHyunChol <johyunchol@JoHyunCholui-iMac.local>
2022-04-14 00:46:38 +0900

    계좌 UI

--- lib/src/toss/account_page.dart
+++ lib/src/toss/account_page.dart
@@ -9,9 +9,36 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.keyboard_arrow_down),
+                label: Text('은행 선택'),
+                enabled: false
+              ),
+            ),
+
+            SizedBox(height: 16),
+
+            TextFormField(
+              decoration: InputDecoration(
+                  label: Text('계좌번호 입력'),
+                  enabled: false
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
