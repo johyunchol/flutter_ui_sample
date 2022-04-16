1abc03bbae872d8f00099070118dd1eb57832e1c
JoHyunChol <johyunchol@JoHyunCholui-iMac.local>
2022-04-17 00:28:34 +0900

    type1 구현 완료

---
+++ lib/src/wanted/type_1.dart
@@ -0,0 +1,127 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Type1 extends StatefulWidget {
+  const Type1({Key? key}) : super(key: key);
+
+  @override
+  State<Type1> createState() => _Type1State();
+}
+
+class _Type1State extends State<Type1> {
+  late PageController _pageController;
+
+  List<Color> pageColors = [Colors.blue, Colors.green, Colors.black, Colors.orange, Colors.cyan];
+
+  Timer? timer;
+
+  int currentIndex = 0;
+
+  _timer() async {
+    timer = Timer.periodic(const Duration(seconds: 5), (timer) async {
+      setState(() {
+        _pageController.nextPage(duration: Duration(milliseconds: 400), curve: Curves.easeIn);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: pageColors.length * 100, viewportFraction: 0.8);
+    _timer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AppBar(
+          backgroundColor: pageColors[currentIndex],
+          shadowColor: Colors.transparent,
+          leading: SizedBox(),
+          leadingWidth: 0,
+          title: Text('wanted', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900, fontSize: 30)),
+          actions: [
+            Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.search_outlined, size: 30)),
+            Stack(
+              children: [
+                Center(child: Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.add_alert_outlined))),
+                Positioned(
+                    top: 12,
+                    right: 8,
+                    child: Container(
+                      width: 4,
+                      height: 4,
+                      decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+                    ))
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 30,
+                height: 30,
+                decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+              ),
+            )
+          ],
+        ),
+        const Divider(height: 0.5, color: Colors.grey),
+        Container(
+          width: double.infinity,
+          color: pageColors[currentIndex],
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: Text('EVENT', style: TextStyle(color: pageColors[currentIndex], fontWeight: FontWeight.w900)),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(4), color: Colors.white),
+              ),
+              SizedBox(height: 8),
+              Text("'더 나은 리더'가 되는 법", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24)),
+              SizedBox(height: 8),
+              Text("[무료] 리더십 아티클, 영상 공개", style: TextStyle(fontSize: 16))
+            ],
+          ),
+        ),
+        Stack(
+          children: [
+            Container(height: 120, color: pageColors[currentIndex]),
+            Container(
+              height: 140,
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (index) {
+                  setState(() {
+                    currentIndex = index % pageColors.length;
+                  });
+                },
+                itemBuilder: (BuildContext context, int index) {
+                  return _item(color: pageColors[index % pageColors.length]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _item({Color? color}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Container(decoration: BoxDecoration(color: color, borderRadius: BorderRadius.circular(8))),
+    );
+  }
+}
--- lib/src/wanted/wanted_home.dart
+++ lib/src/wanted/wanted_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample/src/wanted/type_1.dart';
 
 class WantedHome extends StatefulWidget {
   const WantedHome({Key? key}) : super(key: key);
@@ -8,9 +9,6 @@ class WantedHome extends StatefulWidget {
 }
 
 class _WantedHomeState extends State<WantedHome> {
-  PageController _pageController = PageController(viewportFraction: 0.8);
-
-  Color backgroundColor = Colors.deepPurple;
 
   @override
   Widget build(BuildContext context) {
@@ -19,70 +17,7 @@ class _WantedHomeState extends State<WantedHome> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            AppBar(
-              backgroundColor: backgroundColor,
-              shadowColor: Colors.transparent,
-              leading: SizedBox(),
-              leadingWidth: 0,
-              title: Text('wanted', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w900, fontSize: 30)),
-              actions: [
-                Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.search_outlined, size: 30,)),
-                Stack(
-                  children: [
-                    Center(child: Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.add_alert_outlined))),
-                    Positioned(
-                        top: 12,
-                        right: 8,
-                        child: Container(
-                          width: 4,
-                          height: 4,
-                          decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-                        ))
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 30,
-                    height: 30,
-                    decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-                  ),
-                )
-              ],
-            ),
-            const Divider(height: 0.5, color: Colors.grey),
-
-            Container(
-              width: double.infinity,
-              color: backgroundColor,
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-                    child: Text('EVENT', style: TextStyle(color: backgroundColor, fontWeight: FontWeight.w900),),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                      color: Colors.white,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              height: 240,
-              color: backgroundColor,
-              child: PageView(
-                controller: _pageController,
-                children: [
-                  Container(decoration: BoxDecoration(color: Colors.red, borderRadius: BorderRadius.circular(50))),
-                  Container(decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(50))),
-                  Container(decoration: BoxDecoration(color: Colors.green, borderRadius: BorderRadius.circular(50))),
-                  Container(decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(50))),
-                  Container(decoration: BoxDecoration(color: Colors.orange, borderRadius: BorderRadius.circular(50))),
-                  Container(decoration: BoxDecoration(color: Colors.cyan, borderRadius: BorderRadius.circular(50))),
-                ],
-              ),
-            ),
+            Type1(),
           ],
         ),
       ),
