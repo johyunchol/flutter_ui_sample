3552a0a3b721cab820b017c69955d5d9fc522df7
JoHyunChol <johyunchol@JoHyunCholui-iMac.local>
2022-04-17 22:35:12 +0900

    bottomNavigation

--- lib/src/wanted/wanted_home.dart
+++ lib/src/wanted/wanted_home.dart
@@ -21,6 +21,21 @@ class _WantedHomeState extends State<WantedHome> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: TextStyle(color: Colors.white),
+        unselectedLabelStyle: TextStyle(color: Colors.grey),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: '채용'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat_outlined), label: '커뮤니티'),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle_outlined), label: '내 정보'),
+          BottomNavigationBarItem(icon: Icon(Icons.watch_later_outlined), label: 'MY 원티드'),
+        ],
+      ),
     );
   }
 }
